637c013d9d20129e479ecff2eff6fc527f0d23ed
Aye <Aye@AYE-PC>
2016-10-21 20:09:44 -0300

    falta interface y metodos

---
+++ Ej6/iRepositorioUsuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej6
+{
+    public interface IRepositorioUsuarios
+    {
+
+        void Agregar(Usuario pUsuario);
+
+        void Actualizar(Usuario pUsuario);
+
+        void Eliminar(string pCodigo);
+
+        IList<Usuario> ObtenerTodos();
+
+        Usuario ObtenerPorCodigo(string pCodigo);
+
+         IList<Usuario> ObtenerOrdenadosPor(IComparer<Usuario> pComparador);
+        
+    }
+}
