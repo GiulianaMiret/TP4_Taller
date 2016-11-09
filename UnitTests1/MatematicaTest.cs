e5b2f33ffb148e70a2f68d3bca9fbf7c9677bc9e
GiulianaMiret <dariela_77@hotmail.com>
2016-11-09 19:48:32 -0300

    Test del Ej 2

---
+++ UnitTests1/MatematicaTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ej2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit.Tests
+{
+    [TestClass()]
+    public class MatematicaTests
+    {
+        [TestMethod()]
+        public void DividirTest()
+        {
+            
+            Assert.AreEqual(3, Matematica.Dividir(6, 2));
+        }
+
+        [TestMethod()]
+        public void DividirPorCeroTest()
+        {
+            try
+            {
+                Matematica.Dividir(5, 0);
+                ///Assert.IsFalse(true);
+            }
+            catch (Exception)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
